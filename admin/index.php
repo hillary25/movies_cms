c7a7167779af3e2383f56357e7d40c9365efdc38
Hillary <h_strong69707@fanshaweonline.ca>
2020-03-22 21:34:56 -0400

    Can add movie

--- admin/index.php
+++ admin/index.php
@@ -18,6 +18,7 @@
     <a href="admin_createuser.php">Create User</a>
     <a href="admin_edituser.php">Edit User</a>
     <a href="admin_deleteuser.php">Delete User</a>
+    <a href="admin_addmovie.php">Add Movie</a>
     <a href="admin_logout.php">Sign Out</a>
     
 </body>
